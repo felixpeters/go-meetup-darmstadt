3923c52cb3109fb56e91a58353f55d2977cd64df
Felix Peters <felix@albrechtpeters.de>
2017-09-11 13:40:26 +0200

    Basic home page and result template

--- main.go
+++ main.go
@@ -2,33 +2,38 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+// Page represents a webpage. Body can contain a string or HTML.
 type Page struct {
 	Title string
 	Body  []byte
 }
 
+type Result struct {
+	Result float64
+	Error  string
+}
+
+// homeHandler serves the HTML containing instructions for app usage.
 func homeHandler(w http.ResponseWriter, req *http.Request) {
-	page, err := loadPage("home")
-	if err != nil {
-		io.WriteString(w, "unable to load page")
-	}
-	fmt.Fprintf(w, "%s", page.Body)
+	displayPage("home", w)
 }
 
+// sineHandler displays the result of the sine operation for the given input.
 func sineHandler(w http.ResponseWriter, req *http.Request) {
-	page, err := loadPage("sine")
-	if err != nil {
-		io.WriteString(w, "unable to load page")
+	r := Result{
+		Result: 0.123456,
 	}
-	fmt.Fprintf(w, "%s", page.Body)
+	displayResult(r, w)
 }
 
+// loadPage retrieves the HTML file with the given title. Title should match
+// filename.
 func loadPage(title string) (*Page, error) {
 	filename := "html/" + title + ".html"
 	body, err := ioutil.ReadFile(filename)
@@ -36,6 +41,27 @@ func loadPage(title string) (*Page, error) {
 		return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
+}
+
+func displayResult(r Result, w http.ResponseWriter) {
+	t, err := template.ParseFiles("html/result.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = t.Execute(w, r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// displayPage loads the page with the given title and writes its content to
+// w. An error message is displayed if an error occurs.
+func displayPage(title string, w http.ResponseWriter) {
+	page, err := loadPage(title)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, "%s", page.Body)
 }
 
 func main() {
