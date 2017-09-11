bc67b27d0d6a142ae2407cc73c53a5becebbfc1a
Felix Peters <felix@albrechtpeters.de>
2017-09-11 08:57:39 +0200

    Working web server with two basic views

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func homeHandler(w http.ResponseWriter, req *http.Request) {
+	page, err := loadPage("home")
+	if err != nil {
+		io.WriteString(w, "unable to load page")
+	}
+	fmt.Fprintf(w, "%s", page.Body)
+}
+
+func sineHandler(w http.ResponseWriter, req *http.Request) {
+	page, err := loadPage("sine")
+	if err != nil {
+		io.WriteString(w, "unable to load page")
+	}
+	fmt.Fprintf(w, "%s", page.Body)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := "html/" + title + ".html"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
 
 func main() {
-	fmt.Println("vim-go")
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/sine", sineHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
